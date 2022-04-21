2daaefa91bfb40e10720f631a9a0b1dc9fb118fd
Shakhaowat Hossain Sazib <shsazibbhuiyan892939@gmail.com>
2022-04-21 18:03:09 +0600

    Login Page Using Tailwind CSS

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+      boxShadow: {
+        'inner-input': 'inset -3px -3px 9px 1px #fff, inset 3px 5px 8px -6px black',
+        'date': "-2px -2px 9px 2px #fff, 3px 3px 9px -3px black"
+      }
+    },
+  },
+  plugins: [],
+}
